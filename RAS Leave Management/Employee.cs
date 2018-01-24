1fd03ca4c5f005ee50bfa0fb8a12b2f70c128a9b
Carlo Renoria <itechcarlo@gmail.com>
2018-01-24 10:22:45 +0800

    1.0.5.0

--- RAS Leave Management/Employee.cs
+++ RAS Leave Management/Employee.cs
@@ -30,7 +30,6 @@ namespace RAS_Leave_Management
 
         private void Employee_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("dfasfsadf");
             updater.DoUpdate();
             month();
             cn.Close();
